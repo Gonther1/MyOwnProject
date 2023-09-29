4a4c311f19768e496340beb53fb07c778d5059e8
Gonther1 <126286883+Gonther1@users.noreply.github.com>
2023-09-29 11:40:38 -0500

    feat: :tada: New Project MyOwnProject
    @Gonther1

---
+++ Infrastructure/Data/MyOwnProjectContext.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class MyOwnProjectContext 
+    {
+        public MyOwnProjectContext()
+        {
+        }
+    }
+}
