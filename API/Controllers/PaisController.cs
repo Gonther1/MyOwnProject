be73581100825ea72be9c0dbe320b3d91041ecfe
Andrés Alvarez <126286883+Gonther1@users.noreply.github.com>
2023-10-06 16:41:27 +0000

    feat: :sparkles: Method Get by Id in webapi
    @Gonther1

--- API/Controllers/PaisController.cs
+++ API/Controllers/PaisController.cs
@@ -27,4 +27,15 @@ public class PaisController : BaseController
         var paises = await _context.Paises.ToListAsync();
         return Ok(paises);
     }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+    public async Task<ActionResult> Get(string id)
+    {
+        var pais = await _context.Paises.FindAsync(id);
+        return Ok(pais);
+    }
+
 }
